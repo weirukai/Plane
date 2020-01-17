55671965d685b641f4f3a9118d077605f5cda863
weiruyi <1581778251@qq.com>
2020-01-17 21:05:16 +0800

    aa

--- ThunderCross/Code/ImagesClass.py
+++ ThunderCross/Code/ImagesClass.py
@@ -4,7 +4,7 @@ from pygame import image
 
 class Images(pygame.sprite.Sprite):
     def __init__(self, image_path: str):
-        super.__init__()  # 子类的构造方法的第一行要调用父类的构造方法
+        super().__init__()  # 子类的构造方法的第一行要调用父类的构造方法
         self.image = pygame.image.load(image_path)  # 添加一个属性图片（image是pygame的一个类）
         self.rect = self.image.get_rect()  # 获得这个图片的rect属性（rect是pygame提供的一个属性）
 
