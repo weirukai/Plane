32af377484246735a776a7215c7b2419523c91ba
weirukai <15623739350@163.com>
2020-01-17 16:30:20 +0800

    开始绘图线程

--- ThunderCross/Code/Main.py
+++ ThunderCross/Code/Main.py
@@ -5,13 +5,30 @@
 """
 import pygame
 from code import *
-
+import time
 """
 定义全局常量
 """
 PLAYER_HP = 200
 ENEMY_HP = 50
+# 定义屏幕常量
+SCREEN_RECT = pygame.Rect(30, 0, 700, 480)
+
+
+class Game(object):
+    def __init__(self):
+        self._screenWidth = 300
+        self._screenHeight = 600
+        pygame.display.set_mode(SCREEN_RECT.size)
+
+    def startGame(self):
+        pygame.init()
+        while True:
+            pygame.display.update()
+            time.sleep(200)
 
 
 if __name__ == '__main__':
     print("游戏开始的敌方")
+    game = Game()
+    game.startGame()
